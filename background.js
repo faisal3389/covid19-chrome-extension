1e2588e7ac93d7da59d2c1a6076d47d32b862ad5
Maverick <afaisal3389@gmail.com>
2020-04-06 04:16:50 +0530

    updated prettier, removed cipboard an added the total cases API

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is green.");
-    chrome.browserAction.setBadgeText({ text: "10+" });
-  });
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.covid19india.org/data.json', true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var result = JSON.parse(this.response);
+
+        if (request.status >= 200 && request.status < 400) {
+            const [data] = result.cases_time_series.slice(-1);
+            if (data && data.totalconfirmed) {
+                chrome.storage.sync.set(
+                    { totalCases: data.totalconfirmed },
+                    function () {
+                        console.log(
+                            'The total cases in india = ',
+                            data.totalconfirmed
+                        );
+                        chrome.browserAction.setBadgeText({
+                            text: `${data.totalconfirmed}`,
+                        });
+                    }
+                );
+            }
+        } else {
+            console.log('error');
+        }
+    };
+    request.send();
 });
